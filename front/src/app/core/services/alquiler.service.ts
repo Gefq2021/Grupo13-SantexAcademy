e4295c800f34a697cb93dd97cdcb711902fa8de1
koritacrah <juanmcoro2003@gmail.com>
2023-10-10 11:04:49 -0300

    alquiler service
    creacion del service para gestionar alquileres

--- front/src/app/core/services/alquiler.service.ts
+++ front/src/app/core/services/alquiler.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from '../http/api.service';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlquilerService {
+  mainURL:string = "http://localhost:4001/alquiler/"
+  constructor(private _http:ApiService) { }
+  public postAlquiler(body: any): Observable<any> {
+    return this._http.post(this.mainURL,body);
+  }
+  public getAlquileres(): Observable<any> {
+     return this._http.get(this.mainURL);
+  }
+    public getbyId(id:string): Observable<any> {
+    return this._http.get(this.mainURL+id);
+    }
+    public deleteAlquiler(id:string): Observable<any> {
+       return this._http.delete(this.mainURL+id);
+    }
+    public putAlquiler(id:string,body:any): Observable<any> {
+      return this._http.put(this.mainURL+id,body);
+    }
 
-  constructor() { }
+
 }
